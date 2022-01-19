5f77a2378daf645e3757627b8f5e92462713d5e1
Hannah Hill <you@example.com>
2022-01-18 21:29:33 -0500

    flask app

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
-from flask import Flask, render_template
-from logging import debug
-from flask_pymongo import PyMongo
-from werkzeug.utils import append_slash_redirect, redirect
-from script import api_call
-import easygui
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+# from logging import debug
+# from flask_pymongo import PyMongo
+# from werkzeug.utils import append_slash_redirect, redirect
+import pickle
+# import easygui
 
 app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/city_weather_db")
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/city_weather_db")
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/script")
-def script():
-    api_call()
-    easygui.msgbox("Database Updated Successfully\nCheck Your Mongo To See Our Raw Data", "Success")
-    return redirect("/")
+@app.route("/predict", methods=['POST'])
+def predict():
+    init_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(init_features)]
+    prediction = model.predict(final_features)
+    return render_template('index.html', prediction_text = f"Predicted Class: {prediction}")
 
 
 if __name__ == '__main__':
--- script.py
+++ script.py
@@ -1,99 +1,29 @@
 import pandas as pd
-import numpy as np
-import requests
-import time
-from config import api_key
-import pymongo
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
-def api_call():
-    #connect to mongo db
-    client = pymongo.MongoClient("mongodb://localhost:27017")
+df = pd.read_csv('Resources/merged_dedup.csv')
 
-    #getting city list of top 200 cities
+X = df.drop(columns = ['artist_name', 'track_name', 'track_id', 'top_200'])
+y = df['top_200']
 
-    cities = pd.read_csv(r"static\resources\cities.csv")
-    cities.head()
+X_dummy = pd.get_dummies(X)
+X_dummy.head()
 
-    city_list = []
+X_train, X_test, y_train, y_test = train_test_split(X_dummy, y, random_state=42)
 
-    for city in cities['name']:
-        city_list.append(city)
+# Create a StandardScaler instances
+scaler = StandardScaler()
 
-    #first api call for current weather data
+# Fit the StandardScaler
+X_scaler = scaler.fit(X_train)
 
-    url = 'http://api.openweathermap.org/data/2.5/weather?'
-    units = 'metric'
-    query_url = f'{url}appid={api_key}&units={units}&q='
+# Scale the data
+X_train_scaled = X_scaler.transform(X_train)
+X_test_scaled = X_scaler.transform(X_test)
 
-    city_name = []
-    lat = []
-    lng = []
-    max_temp = []
-    humidity = []
-    cloudiness = []
-    wind_speed = []
-    country = []
-    date = []
-    for city in city_list:
-        response = requests.get(query_url + city).json()
-        city_name.append(city)
-        lat.append(response['coord']['lat'])
-        lng.append(response['coord']['lon'])
-        max_temp.append(response['main']['temp_max'])
-        humidity.append(response['main']['humidity'])
-        wind_speed.append(response['wind']['speed'])
-        country.append(response['sys']['country'])
-        cloudiness.append(response['clouds']['all'])
-        date.append(response['dt'])
+clf = RandomForestClassifier(random_state=1, n_estimators=500).fit(X_train_scaled, y_train)
 
-    #creating dataframe and saving to new csv when called
-
-    weather = {'City':city_name,'Lat':lat,'Lng':lng,'Max Temp':max_temp,'Humidity':humidity,'Cloudiness':cloudiness,'Wind Speed':wind_speed,'Country':country,'Date':date}
-    weather_df = pd.DataFrame({ key:pd.Series(value) for key, value in weather.items() })
-    weather_df.to_csv(r'static\resources\weather_df.csv', index=False)
-
-    #update mongo db with new data
-
-    weather_data = weather_df.to_dict(orient='records')
-    db = client["city_weather_db"]
-    db.weather.insert_many(weather_data)
-
-    #second api call for 5 day forecast
-
-    url = 'http://api.openweathermap.org/data/2.5/forecast?'
-    units = 'metric'
-    query_url = f'{url}appid={api_key}&units={units}&q='
-
-
-    city_name = []
-    lat_5 = []
-    lng_5 = []
-    max_temp_5 = []
-    humidity_5 = []
-    cloudiness_5 = []
-    wind_speed_5 = []
-    date_5 = []
-
-
-    for city in city_list:
-        for i in range(0,40,8):
-                response = requests.get(query_url + city).json()
-                city_name.append(city)
-                lat_5.append(response['city']['coord']['lat'])
-                lng_5.append(response['city']['coord']['lon'])
-                max_temp_5.append(response['list'][i]['main']['temp_max'])
-                humidity_5.append(response['list'][i]['main']['humidity'])
-                wind_speed_5.append(response['list'][i]['wind']['speed'])
-                cloudiness_5.append(response['list'][i]['clouds']['all'])
-                date_5.append(response['list'][i]['dt_txt'])
-
-    #creating dataframe and saving to new csv when called
-    weather_5 = {'City':city_name,'Lat':lat_5,'Lng':lng_5,'Max Temp':max_temp_5,'Humidity':humidity_5,'Cloudiness':cloudiness_5,'Wind Speed':wind_speed_5,'Date':date_5}
-    weather5_df = pd.DataFrame({ key:pd.Series(value) for key, value in weather_5.items() })
-    weather5_df.to_csv(r'static\resources\weather_5.csv', index=False)
-
-    #update mongo db with new data
-    data5 = weather5_df.to_dict(orient='records')
-    db = client["city_weather_db"]
-    db.weather_5.insert_many(data5)
-
+pickle.dump(clf, open('model.pkl', 'wb'))
