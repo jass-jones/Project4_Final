8ec8763f2e3067c2e73d9d1a2fafe4c0f181e279
Hannah Hill <you@example.com>
2022-01-22 11:43:54 -0500

    updated app with week prediction

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pickle
 # import easygui
 
 app = Flask(__name__)
+classifier = pickle.load(open('classifier.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
 # mongo = PyMongo(app, uri="mongodb://localhost:27017/city_weather_db")
@@ -19,12 +20,18 @@ def home():
 def predict():
     init_features = [float(x) for x in request.form.values()]
     final_features = [np.array(init_features)]
-    prediction = model.predict(final_features)
-    if prediction == 1:
-        return render_template('index.html', prediction_text = f"This song is predicted to make the Spotify Top 200 Charts!")
+    prediction1 = classifier.predict(final_features)
+    prediction2 = model.predict(final_features)
+    if prediction1 == 1 and prediction2 == 0:
+        return render_template('index.html', prediction1_text = f"This song is predicted to make the Spotify Top 200 Charts!", prediction2_text = f"It will likely be on the charts for 1-4 weeks.")
+    elif prediction1 == 1 and prediction2 == 1:
+        return render_template('index.html', prediction1_text = f"This song is predicted to make the Spotify Top 200 Charts!", prediction2_text = f"It will likely be on the charts for 5-24 weeks.")
+    elif prediction1 == 1 and prediction2 == 2:
+        return render_template('index.html', prediction1_text = f"This song is predicted to make the Spotify Top 200 Charts!", prediction2_text = f"It will likely be on the charts for 25-52 weeks.")
+    elif prediction1 == 1 and prediction2 == 3:
+        return render_template('index.html', prediction1_text = f"This song is predicted to make the Spotify Top 200 Charts!", prediction2_text = f"It will likely be on the charts for over a year!")
     else:
-        return render_template('index.html', prediction_text = f"This song is not predicted to make the Spotify Top 200 Charts.")
-
+        return render_template('index.html', prediction1_text = f"This song is not predicted to make the Spotify Top 200 Charts.")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- script.py
+++ script.py
@@ -2,27 +2,36 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 import pickle
 
+## BUILD MODEL FOR YES/NO CHARTING
 df = pd.read_csv('Resources/merged_dedup.csv')
-
 X = df.drop(columns = ['artist_name', 'track_name', 'track_id', 'top_200', 'key']).dropna()
 y = df['top_200'].dropna()
-
 # X_dummy = pd.get_dummies(X)
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-
 # Create a StandardScaler instances
 scaler = StandardScaler()
-
 # Fit the StandardScaler
 X_scaler = scaler.fit(X_train)
-
 # Scale the data
 X_train_scaled = X_scaler.transform(X_train)
 X_test_scaled = X_scaler.transform(X_test)
-
 clf = RandomForestClassifier(random_state=1, n_estimators=500).fit(X_train_scaled, y_train)
+pickle.dump(clf, open('classifier.pkl', 'wb'))
 
+## BUILD MODEL FOR HOW MANY WEEKS WILL CHART
+df2 = pd.read_csv('Resources/top200_cleaned.csv')
+X = df2.drop(columns = ['artist_name', 'track_name', 'track_name', 'track_id', 'time_charted'])
+y = df2['time_charted']
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+# Create a StandardScaler instances
+scaler = StandardScaler()
+# Fit the StandardScaler
+X_scaler = scaler.fit(X_train)
+# Scale the data
+X_train_scaled = X_scaler.transform(X_train)
+X_test_scaled = X_scaler.transform(X_test)
+knn = KNeighborsClassifier(n_neighbors=9).fit(X_train_scaled, y_train)
 pickle.dump(clf, open('model.pkl', 'wb'))
